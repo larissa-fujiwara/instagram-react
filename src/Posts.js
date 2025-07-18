c09b17c102488b4439604208bdb722070ac71ebe
fuji <lrs.fujiwara@gmail.com>
2025-07-18 19:07:24 -0300

    Implementação do toggle do icone salvar post

--- src/Posts.js
+++ src/Posts.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 export default function Posts() {
     const posts = [
         { imgUsuario: "assets/img/meowed.svg", usuario: "meowed", imgPost: "assets/img/gato-telefone.svg", imgPerfilCurtido: "assets/img/respondeai.svg", usuarioCurtido: "respondeai" },
@@ -13,42 +15,65 @@ export default function Posts() {
     );
 }
 
+function SalvarPost() {
+
+    const [estado, setEstado] = React.useState(false);
+
+       
+    function salvarPublicacao(){
+        const novoEstado = inverteEstado(estado);
+        setEstado(novoEstado);
+        console.log(novoEstado);
+
+    }
+
+    function inverteEstado(estadoAtual){
+        const toggle = !estadoAtual;
+        return toggle;
+    }
+
+    return (
+        <ion-icon onClick={salvarPublicacao} name={estado ? "bookmark-sharp" : "bookmark-outline"}></ion-icon>
+    )
+}
+
 function Post(props) {
-    return(
-            <div class="post">
-        <div class="topo">
-            <div class="usuario">
-                <img src={props.imgUsuario} alt="meowed" />
-                {props.usuario}
-            </div>
-            <div class="acoes">
-                <ion-icon name="ellipsis-horizontal"></ion-icon>
-            </div>
-        </div>
 
-        <div class="conteudo">
-            <img src={props.imgPost} alt="gato-telefone" />
-        </div>
-
-        <div class="fundo">
-            <div class="acoes">
-                <div>
-                    <ion-icon name="heart-outline"></ion-icon>
-                    <ion-icon name="chatbubble-outline"></ion-icon>
-                    <ion-icon name="paper-plane-outline"></ion-icon>
+    return (
+        <div class="post">
+            <div class="topo">
+                <div class="usuario">
+                    <img src={props.imgUsuario} alt="meowed" />
+                    {props.usuario}
                 </div>
-                <div>
-                    <ion-icon name="bookmark-outline"></ion-icon>
+                <div class="acoes">
+                    <ion-icon name="ellipsis-horizontal"></ion-icon>
                 </div>
             </div>
 
-            <div class="curtidas">
-                <img src={props.imgPerfilCurtido} alt="respondeai" />
-                <div class="texto">
-                    Curtido por <strong>{props.usuarioCurtido}</strong> e <strong>outras 101.523 pessoas</strong>
+            <div class="conteudo">
+                <img src={props.imgPost} alt="gato-telefone" />
+            </div>
+
+            <div class="fundo">
+                <div class="acoes">
+                    <div>
+                        <ion-icon name="heart-outline"></ion-icon>
+                        <ion-icon name="chatbubble-outline"></ion-icon>
+                        <ion-icon name="paper-plane-outline"></ion-icon>
+                    </div>
+                    <div>
+                        <SalvarPost />
+                    </div>
+                </div>
+
+                <div class="curtidas">
+                    <img src={props.imgPerfilCurtido} alt="respondeai" />
+                    <div class="texto">
+                        Curtido por <strong>{props.usuarioCurtido}</strong> e <strong>outras 101.523 pessoas</strong>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
     )
 }
