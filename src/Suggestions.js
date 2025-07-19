f113ed734b1ab19ca19f6497019465feb5e64226
fuji <lrs.fujiwara@gmail.com>
2025-07-19 14:04:30 -0300

    Pequenos ajustes no array e propriedade alt da imagem da sugestões de usuario

--- src/Suggestions.js
+++ src/Suggestions.js
@@ -1,10 +1,10 @@
 export default function Suggestions() {
     const sugeridos = [
-        { imagem: "assets/img/bad.vibes.memes.svg", nome: "bad.vibes.memes", razao: "Segue você" },
-        { imagem: "assets/img/chibirdart.svg", nome: "chibirdart", razao: "Segue você" },
-        { imagem: "assets/img/razoesparaacreditar.svg", nome: "razoesparaacreditar", razao: "Novo no Instagram" },
-        { imagem: "assets/img/adorable_animals.svg", nome: "adorable_animals", razao: "Segue você" },
-        { imagem: "assets/img/smallcutecats.svg", nome: "smallcutecats", razao: "Segue você" },
+        { imagem: "assets/img/bad.vibes.memes.svg", nome: "bad.vibes.memes", razao: "Segue você", alt:"bad.vibes.memes" },
+        { imagem: "assets/img/chibirdart.svg", nome: "chibirdart", razao: "Segue você", alt:"chibirdart" },
+        { imagem: "assets/img/razoesparaacreditar.svg", nome: "razoesparaacreditar", razao: "Novo no Instagram", alt:"razoesparaacreditar" },
+        { imagem: "assets/img/adorable_animals.svg", nome: "adorable_animals", razao: "Segue você", alt:"adorable_animals" },
+        { imagem: "assets/img/smallcutecats.svg", nome: "smallcutecats", razao: "Segue você", alt:"smallcutecats" },
 
     ]
 
@@ -14,7 +14,7 @@ export default function Suggestions() {
                 Sugestões para você
                 <div>Ver tudo</div>
             </div>
-            {sugeridos.map(perfil => <Sugerido imagem={perfil.imagem} nome={perfil.nome} razao={perfil.razao} />)}
+            {sugeridos.map(perfil => <Sugerido imagem={perfil.imagem} nome={perfil.nome} razao={perfil.razao} alt={perfil.alt} />)}
         </div>
     )
 }
@@ -23,7 +23,7 @@ function Sugerido(props) {
     return (
         <div class="sugestao">
             <div class="usuario">
-                <img src={props.imagem} alt="bad.vibes.memes.svg" />
+                <img src={props.imagem} alt={props.alt} />
                 <div class="texto">
                     <div class="nome">{props.nome}</div>
                     <div class="razao">{props.razao}</div>
